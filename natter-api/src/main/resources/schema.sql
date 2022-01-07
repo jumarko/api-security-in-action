9a288144a31cff0913ecab1aeb3ffc14595488bb
Juraj Martinka <jumarko@gmail.com>
2022-01-07 13:51:00 +0100

    Create project skeleton.

---
+++ natter-api/src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE  spaces(
+    space_id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    owner VARCHAR(255) NOT NULL
+);
+
+CREATE SEQUENCE space_id_seq;
+
+CREATE TABLE  messages(
+    space_id INT NOT NULL REFERENCES spaces(space_id),
+    msg_id INT PRIMARY KEY,
+    author VARCHAR(30) NOT NULL,
+    msg_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    msg_text VARCHAR(1024) NOT NULL
+);

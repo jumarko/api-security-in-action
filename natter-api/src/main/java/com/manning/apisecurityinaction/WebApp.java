fa75505eb3d96750d29679475977fda11579d4d0
Juraj Martinka <jumarko@gmail.com>
2022-01-10 07:03:43 +0100

    Chapter 2: safe outputs - don't leak server info

--- natter-api/src/main/java/com/manning/apisecurityinaction/WebApp.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/WebApp.java
@@ -30,7 +30,7 @@ public class WebApp {
         // otherwise you'll get text/html content type for error responses
         // -> see page 37 about Content-Type
         // This commit fixes it anyway: https://github.com/NeilMadden/apisecurityinaction/commit/067b05a72fe8ed92b09d545912e8a33f8a909ab5#diff-eae019c32d4ba4dda402c532030540ae66b80cc7f7687fe353766126427d5814
-        Spark.afterAfter(((request, response) -> response.type("application/json")));
+        Spark.afterAfter((request, response) -> response.type("application/json"));
 
         Spark.internalServerError(new JSONObject()
                 .put("error", "internal server error").toString());
@@ -39,6 +39,9 @@ public class WebApp {
         Spark.exception(IllegalArgumentException.class, WebApp::badRequest);
         Spark.exception(JSONException.class, WebApp::badRequest);
         Spark.exception(EmptyResultException.class, (e, request, response) -> response.status(404));
+
+        // don't leak internal server info
+        Spark.afterAfter((request, response) -> response.header("Server", ""));
     }
 
     private static <T extends Exception> void badRequest(Exception e, Request request, Response response) {

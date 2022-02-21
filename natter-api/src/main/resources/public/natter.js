8f010346a99d9fb2b2a1b113a6ef2218824f3b3f
Juraj Martinka <jumarko@gmail.com>
2022-02-21 14:01:33 +0100

    Chapter 4: Add missing js/html resources.

--- natter-api/src/main/resources/public/natter.js
+++ natter-api/src/main/resources/public/natter.js
@@ -2,18 +2,25 @@ const apiUrl = 'https://localhost:4567'
 
 function createSpace(name, owner) {
   const data = {name, owner};
+  // csrtToken cookie is set upon successful login - see login.js
+  const crsfToken = getCookie("csrfToken");
 
   fetch(apiUrl  + '/spaces', {
     method: 'POST',
     credentials: 'include',
     body: JSON.stringify(data),
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      // make sure to include anti-CSRF token in the header
+      'X-CSRF-Token': crsfToken
     }
   })
     .then(response => {
       if (response.ok) {
         return response.json();
+      } else if (response.status === 401) {
+        // redirect unauthorized users to the Login UI
+        window.location.replace('/login.html');
       } else {
         throw Error(response.statusText + ": " + response.text().then(alert));
       }
@@ -32,4 +39,14 @@ function processFormSubmit(e) {
   const owner = document.getElementById('owner').value;
   createSpace(spaceName, owner);
   return false; // prevent further event processing
+}
+
+function getCookie(cookieName) {
+  const cookieValue = document.cookie.split(';')
+    .map(item => item.split('=')
+      .map(x => decodeURIComponent(x.trim())))
+    .filter(item => item[0] === cookieName)[0];
+  if (cookieValue) {
+    return cookieValue[1];
+  }
 }
